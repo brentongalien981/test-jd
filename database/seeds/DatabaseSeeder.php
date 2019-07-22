10643a52693c0024e241760551283ffc21e6dd28
brentongalien981 <brentongalien@gmail.com>
2019-07-21 23:10:21 -0400

    Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        //
+        $photoLink1 = "https://live.staticflickr.com/65535/48340808017_52be9c008b_c.jpg";
+        $photoLink2 = "https://www.youtube.com/embed/tvTRZJ-4EyI";
+
+
+
+        //
+        $data = array(
+            array("id" => null, "title" => null, "src" => $photoLink1, "created_at" => null, "updated_at" => null)
+        );
+
+
+        // Insert some stuff
+        DB::table('photos')->insert($data);
     }
 }
